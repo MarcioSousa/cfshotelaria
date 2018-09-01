ca20fa1ddf8b45b75efe9594cec44293bb42907e
Marcio Sousa <marciofrapeso@gmail.com>
2018-09-01 16:39:33 -0300

    Splash, Login e Principal Funcionando

---
+++ Control/Cls_AcessoMySqlCadFacil.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Control.Properties;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace Control
+{
+    public class Cls_AcessoMySqlCadFacil
+    {
+        private MySqlConnection CriarConexao()
+        {
+            return new MySqlConnection(Settings.Default.stringConexaoCadFacil);
+        }
+        private MySqlParameterCollection mySqlParameterCollection = new MySqlCommand().Parameters;
+        public void LimparParametros()
+        {
+            mySqlParameterCollection.Clear();
+        }
+        public void AdicionarParametros(string nomeParametro, object valorParametro)
+        {
+            mySqlParameterCollection.Add(new MySqlParameter(nomeParametro, valorParametro));
+        }
+        public DataTable ExecutarConsulta(CommandType commandType, string nomeStoreProcedureOuTextoSql)
+        {
+            try
+            {
+                MySqlConnection mySqlConnection = CriarConexao();
+                mySqlConnection.Open();
+                MySqlCommand mySqlCommand = mySqlConnection.CreateCommand();
+                mySqlCommand.CommandType = commandType;
+                mySqlCommand.CommandText = nomeStoreProcedureOuTextoSql;
+                mySqlCommand.CommandTimeout = 7200;
+
+                foreach (MySqlParameter mySqlParameter in mySqlParameterCollection)
+                {
+                    mySqlCommand.Parameters.Add(new MySqlParameter(mySqlParameter.ParameterName, mySqlParameter.Value));
+                }
+
+                MySqlDataAdapter mySqlDataAdapter = new MySqlDataAdapter(mySqlCommand);
+                DataTable dataTable = new DataTable();
+                mySqlDataAdapter.Fill(dataTable);
+                mySqlConnection.Clone();
+                return dataTable;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
---
+++ View/FrmPrincipal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace View
+{
+    public partial class FrmPrincipal : Form
+    {
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmPrincipal_Load(object sender, EventArgs e)
+        {
+            FrmSplashScreen splashScreen = new FrmSplashScreen();
+            splashScreen.ShowDialog();
+            FrmAcesso acesso = new FrmAcesso();
+            acesso.ShowDialog();
+
+            if (!acesso.abrirPrincipal)
+            {
+                this.Close();
+            }
+        }
+    }
+}
